912c74845998ccaadff5a79d0470c2588337b473
chris <christophe.vandooren@student.kdg.be>
2018-02-04 14:12:51 +0100

    test

---
+++ src/main/java/be/kdg/blog/controllers/ResponseBodyController.java
@@ -0,0 +1,16 @@
+package be.kdg.blog.controllers;
+
+import be.kdg.blog.model.Blog;
+
+public class ResponseBodyController {
+
+    private Blog blog;
+
+   // public ResponseBodyController(){
+
+   // }
+
+    public ResponseBodyController(Blog blog){
+
+        this.blog = blog;}
+}
