97168ee3fdf9fe14c140a269f0eae141fd4087c5
chris <christophe.vandooren@student.kdg.be>
2018-02-04 14:12:14 +0100

    test

--- src/main/java/be/kdg/blog/Application.java
+++ src/main/java/be/kdg/blog/Application.java
@@ -1,5 +1,11 @@
 package be.kdg.blog;
 
+
+import be.kdg.blog.model.Blog;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,5 +13,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application extends SpringApplication {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+
+Blog blog = (Blog)context.getBean("respBody");
+
+blog.getEntries();
     }
 }
