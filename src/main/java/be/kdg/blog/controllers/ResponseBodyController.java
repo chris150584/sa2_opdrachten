ff8f3a88e820d6159cc0538995a677012e2b5e3f
chris <christophe.vandooren@student.kdg.be>
2018-02-05 02:21:26 +0100

    Vraag 3 nog in te vullen

--- src/main/java/be/kdg/blog/controllers/ResponseBodyController.java
+++ src/main/java/be/kdg/blog/controllers/ResponseBodyController.java
@@ -1,14 +1,22 @@
 package be.kdg.blog.controllers;
 
 import be.kdg.blog.model.Blog;
+import be.kdg.blog.model.BlogEntry;
+import be.kdg.blog.model.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class ResponseBodyController {
+public class ResponseBodyController extends Blog {
+
+    private StringBuilder stringBuilder;
+    private Blog blog;
+
+
 public ResponseBodyController(){}
+
     @GetMapping(value ={"/"}, produces= MediaType.TEXT_PLAIN_VALUE)
     @ResponseBody
     public String getClientText(){
@@ -23,11 +31,40 @@ public ResponseBodyController(){}
         return "<html><head><title>Hello!</title></head><body><h1>Hello, world!</h1></body></html>";
     }
 
+    @GetMapping(value ={"/blog"}, produces= MediaType.TEXT_HTML_VALUE)
+    @ResponseBody
+    public String getBlogText(){
+
+   blog = new ResponseBodyController(blog);
+
+    stringBuilder = new StringBuilder();
+    stringBuilder.append("<html><head><title>Hello!</title></head><body>");
+
+        stringBuilder.append("<H1>").append("My Blog").append("</H1>");
+        for (BlogEntry blogEntry:blog.getEntries()) {
+
+            stringBuilder.append("<H2>").append(blogEntry.getSubject()).append("</H2>");
+
+            for(Tag tags:blogEntry.getTags())
+            {stringBuilder.append("<li>").append(tags.getName()).append("</li>");}
+
+            stringBuilder.append("<p align=\"left\">").append("<span = 3>").append(blogEntry.getMessage()).append("</span>").append("</p>");
+        }
+        stringBuilder.append("</body></html>");
+        return stringBuilder.toString();
+
+    }
+
+    // VERDER in te vullen
+    @GetMapping(value ="{/api/blog/entries/{entryId}}", produces= MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String getJsonText(){
+
+    return "";
+    }
 
 
-    private Blog blog;
-
-   // public ResponseBodyController(){
+    // public ResponseBodyController(){
 
    // }
 
